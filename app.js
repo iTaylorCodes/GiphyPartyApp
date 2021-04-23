628155ab862fbd42f4af1dc236a295ff9a682e3b
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-04-22 19:46:36 -0700

    Completed

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("Let's get this party started!");
+const $input = $('#search');
+const $gifContainer = $('#giph-container');
+
+$('form').on('submit', addGif);
+
+// creates a div with the gif in it based on axios results
+function createDiv(res) {
+	let allResults = res.data.length;
+	if (allResults) {
+		let randomIdx = Math.floor(Math.random() * allResults);
+		let $newDiv = $('<div>', { class: 'gif-div' });
+		let $gif = $('<img>', {
+			src: res.data[randomIdx].images.original.url,
+			class: 'gif-img'
+		});
+		$newDiv.append($gif);
+		$gifContainer.append($newDiv);
+	}
+}
+
+// makes ajax request and clears search input
+async function addGif(e) {
+	e.preventDefault();
+
+	const response = await axios.get(
+		`http://api.giphy.com/v1/gifs/search?q=${$input.val()}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`
+	);
+	console.log(response);
+
+	createDiv(response.data);
+
+	$input.val('');
+}
+
+// removes all images
+$('#remove-btn').on('click', function () {
+	$gifContainer.empty();
+});
